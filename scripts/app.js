a60113a434837c74c7e5dd2c575d2d43b9915e9d
Rick Cid <rick@hispand.com>
2015-04-27 16:49:54 -0600

    Created modals for post and edit page

--- scripts/app.js
+++ scripts/app.js
@@ -17,14 +17,6 @@ app.config(function($routeProvider) {
       templateUrl: 'views/register.html',
       controller: 'AuthController'
     })
-    .when('/post', {
-      templateUrl: 'views/post.html',
-      controller: 'TaskController'
-    })
-    .when('/edit/:taskId', {
-      templateUrl: 'views/edit.html',
-      controller: 'TaskController'
-    })
     .when('/browse', {
       templateUrl: 'views/browse.html',
       controller: 'TaskController'
